bbd844783224581fd0889b770258dacdfffec7a5
Bhavna Pant <bhavna@xeontek.com>
2016-03-04 22:58:31 +0000

    initial commit

---
+++ js/selection.js
@@ -0,0 +1,12 @@
+/**
+ * Created by bhavna on 04/03/2016.
+ */
+
+var
+    items = document.getElementsByClassName('item'),
+    log = document.getElementById('log');
+
+log.innerText = items.length + '"item" elements';
+
+var
+    result = '',
